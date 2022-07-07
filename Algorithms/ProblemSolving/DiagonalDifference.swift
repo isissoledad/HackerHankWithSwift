9bed23d1df0e8d9d09064cc65a13d1601d06228e
Ísis Soledad <isissoledad@gmail.com>
2022-07-07 16:50:18 -0300

    HackerRank's Diagonal Difference challenge solution in swift

---
+++ Algorithms/ProblemSolving/DiagonalDifference.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+func diagonalDifference(arr: [[Int]]) -> Int {
+    var diagonalLeft = 0
+    var diagonalRight = 0
+
+    for i in 0 ..< arr.count {
+        diagonalLeft += arr[i][i]
+        diagonalRight += arr[i][arr.count - 1 - i]
+    }
+    return abs(diagonalLeft - diagonalRight)
+}
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+var arr = [[Int]]()
+
+for _ in 1...n {
+    guard let arrRowTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+    let arrRow: [Int] = arrRowTemp.split(separator: " ").map {
+        if let arrItem = Int($0) {
+            return arrItem
+        } else { fatalError("Bad input") }
+    }
+
+    guard arrRow.count == n else { fatalError("Bad input") }
+
+    arr.append(arrRow)
+}
+
+guard arr.count == n else { fatalError("Bad input") }
+
+let result = diagonalDifference(arr: arr)
+
+fileHandle.write(String(result).data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
+
